28de5f3c0cd258e9e3dea2bf85bced50d3876357
Fxzx mic <54622331+fxzxmic@users.noreply.github.com>
2020-09-22 12:56:15 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
-xcopy /s /i /y ".\AxImp.exe" "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\WinSDK-NetFx40Tools-x86\AxImp.exe"
-rd /s /q ".\AxImp.exe"
+xcopy /s /i /y ".\AxImp.exe" "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\WinSDK-NetFx40Tools-x86\"
+rm /s /q ".\AxImp.exe"
 @echo off
 
 for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
